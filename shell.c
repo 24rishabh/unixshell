4493c61ef73eb6eeb2f0a1c7fb95fae5c2703f92
Rishabh  Shakya <rishabh@Rishabhs-MacBook-Pro-1.local>
2025-12-07 18:13:01 +0530

    Handling exit

--- shell.c
+++ shell.c
@@ -1,16 +1,18 @@
 #include <stdio.h>     
 #include <stdlib.h>    
 #include <sys/types.h>
+#include <string.h>
 
 #define MAX_ARGS 10
 
 // Things to do
+// Parsing
 // strip logic
 // Argument parsing including quoted strings
 // I/O redirection (<, >)
 // Built-ins: cd, exit
 // Handling ctrl+c
-
+// handle exit -> done
 
 // Parsing
 int parse_input(char* input, char** args){
@@ -42,12 +44,19 @@ int main(){
     size_t len = 0;
     char *args[MAX_ARGS];
     // get the input from command line
+    while(1){
     printf("myshell> ");
     ssize_t nread = getline(&input, &len, stdin);
     // tokenize -> convert and store it into array
     int i = parse_input(input, args);
+    if (strcmp(args[0], "exit\n") == 0) {
+        break;
+    }
+
     printf("%d\n",i);
     for (int j = 0; args[j] != NULL; j++) {
     printf("arg[%d] = %s\n", j, args[j]);
     }
+}
+
 }
