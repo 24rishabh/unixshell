75a51702cfc8396caaaee5a8488147e87879a886
Rohit Baraik <rohit.daan20@gmail.com>
2025-12-08 04:03:10 +0000

    instead of strtok implemented own argparser

--- basicCode4.c
+++ basicCode4.c
@@ -1,81 +1,115 @@
 #include<stdio.h>
 #include<string.h>
-#include<sys/wait.h>
-#include<unistd.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
-#define MAX_ARGS 64
 #define MAX_INPUT 1024
+#define MAX_ARGS 64
 #define MAX_PATH 1024
+
+/*
+TODO LIST:
+1. I/O redirection: < and >
+2. Pipelines: cmd1 | cmd2
+3. Background: &
+4. Ctrl-C handling
+*/
+
+void ArgParser(char *input,char *args[]){
+    char *p = input; // walking pointer through input
+    int i = 0; // index into args
+
+    while(*p!='\0'){
+        while(*p==' ' || *p=='\t') p++; // skkiping the whitespaces
+        if(*p=='\0') break;
+
+        //if argument starts with double quote
+        if(*p=='\"'){
+            p++;
+            args[i++] = p;
+            // move until closing qute found
+            while(*p!='\"' && *p!='\0') p++;
+
+            if(*p=='\"'){
+                *p = '\0';
+                p++;
+            }
+        }
+        else{
+            // if the argument is normal word
+            args[i++] = p;
+            while(*p!=' ' && *p!='\t' && *p!='\0') p++;
+            if(*p!='\0'){
+                *p = '\0';
+                p++;
+            }
+        }
+
+        if(i>=MAX_ARGS-1) break;
+    }
+    args[i] = NULL;
+}
 
 int main(){
     char input[MAX_INPUT];
     char *args[MAX_ARGS];
     char cwd[MAX_PATH];
-
+    
     while(1){
-        if(getcwd(cwd,sizeof(cwd))==NULL){ // get the current working directory
-            perror("getcwd failed");
+        if(getcwd(cwd,sizeof(cwd))==NULL){
+            perror("cwd failed");
             printf("tsh$ ");
         }
         else{
             printf("%s$ ",cwd);
         }
-        fflush(stdout); // make sure prompt appears
+        fflush(stdout);
 
-        //1.Read a line
         if(fgets(input,MAX_INPUT,stdin)==NULL){
-            perror("fgets failed\n");
+            perror("fgets failed");
             continue;
         }
 
-        // remove trailing \n from the input
         input[strcspn(input,"\n")] = '\0';
 
-        //2.Built-in: exit
         if(strcmp(input,"exit")==0){
             printf("Byee!!!\n");
             break;
         }
-        
-        //3.split input into args
-        char *token = strtok(input," ");
-        int i = 0;
-        while(token!=NULL && i<MAX_ARGS-1){
-            args[i++] = token;
-            token = strtok(NULL," "); 
-        }
-        args[i] = NULL;
 
-        //3.5. Built-in: cd
+        // char *token = strtok(input," ");
+        // int i = 0;
+        // while(input!=NULL && i<MAX_ARGS-1){
+        //     args[i++] = token;
+        //     token = strtok(NULL," ");
+        // }
+        // args[i] = NULL;
+        ArgParser(input,args);
+        if(args[0]==NULL) continue;
+
         if(strcmp(args[0],"cd")==0){
-            if(args[1]==NULL){
-                fprintf(stderr,"cd missing arguments\n");
-            }
-            else if(chdir(args[1])!=0){
-                perror("cd error");
-            }
+            if(args[1]==NULL) fprintf(stderr,"cd missing argument\n");
+            else if(chdir(args[1])!=0) perror("cd failed");
 
             continue;
         }
 
-        //4.create fork child process
         pid_t pid = fork();
         if(pid==0){
-            //child process
             execvp(args[0],args);
             perror("execvp failed");
             exit(EXIT_FAILURE);
         }
         else if(pid>0){
-            //parent process
             int status;
             waitpid(pid,&status,0);
             printf("exit status: %d\n",status);
         }
         else{
-            //fork failed
             perror("fork failed");
         }
     }
+
+    return 0;
 }
