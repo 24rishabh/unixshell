aa0b60ba95584e33d5c38579e4c3194a63560e27
Rohit Baraik <rohit.daan20@gmail.com>
2025-12-08 15:01:13 +0000

    final from rohit's side

--- basicCode4.c
+++ basicCode4.c
@@ -3,20 +3,12 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/wait.h>
-#include<stdbool.h>   // <-- required for bool, true, false
-#include<fcntl.h>     // <-- required for open(), O_RDONLY, etc.
+#include<stdbool.h>  
+#include<fcntl.h>   
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 #define MAX_PATH 1024
-
-/*
-TODO LIST:
-1. I/O redirection: < and >   <--------DONE
-2. Pipelines: cmd1 | cmd2     <--------DONE
-3. Background: &              <--------DONE
-4. Ctrl-C handling            
-*/
 
 void ArgParser(char *input,char *args[]){
     char *p = input; // walking pointer through input
@@ -129,7 +121,7 @@ bool handlePipeline(char *args[],bool background){
     close(fds[0]);
     close(fds[1]);
 
-    if(!background){
+    if(background==false){
         // wait for both child to finish
         int status;
         waitpid(pid1,&status,0);
@@ -150,134 +142,4 @@ bool backgroundDetect(char *args[]){
         return true;
     }
     return false;
-}
-
-int main(){
-    char input[MAX_INPUT];
-    char *args[MAX_ARGS];
-    char cwd[MAX_PATH];
-    
-    while(1){
-        if(getcwd(cwd,sizeof(cwd))==NULL){
-            perror("cwd failed");
-            printf("tsh$ ");
-        }
-        else{
-            printf("%s$ ",cwd);
-        }
-        fflush(stdout);
-
-        if(fgets(input,MAX_INPUT,stdin)==NULL){
-            perror("fgets failed");
-            continue;
-        }
-
-        input[strcspn(input,"\n")] = '\0';
-
-        if(strcmp(input,"exit")==0){
-            printf("Byee!!!\n");
-            break;
-        }
-
-        ArgParser(input,args);
-        if(args[0]==NULL) continue;
-
-        if(strcmp(args[0],"cd")==0){
-            if(args[1]==NULL) fprintf(stderr,"cd missing argument\n");
-            else if(chdir(args[1])!=0) perror("cd failed");
-
-            continue;
-        }
-        // ----- detect bacground present or not -----
-        bool background = backgroundDetect(args);
-
-        // ----- Single pipeline support (cmd1 | cmd2) ---
-        bool pipe = handlePipeline(args,background);
-        if(pipe==true) continue;
-
-        // ----- I/O redirection parsing -----
-        int inDirect = 0, outDirect = 0;
-        char *infile = NULL;
-        char *outfile = NULL;
-        char *cleanArgs[MAX_ARGS];
-        int j = 0;
-        bool parseError = false;
-
-        for(int k=0;args[k]!=NULL;k++){
-            if(strcmp(args[k],"<")==0){
-                if(args[k+1]==NULL){
-                    fprintf(stderr,"Syntax Error: no input file present\n");
-                    parseError = true;
-                    break;
-                }
-                inDirect = 1;
-                infile = args[k+1];
-                k++; // skip the filename
-            }
-            else if(strcmp(args[k],">")==0){
-                if(args[k+1]==NULL){
-                    fprintf(stderr,"Syntax Error: no output file present\n");
-                    parseError = true;
-                }
-                outDirect = 1;
-                outfile = args[k+1];
-                k++; //skip out filename
-            }
-            else{
-                cleanArgs[j++] = args[k];
-            }
-        }
-
-        if(parseError==true) continue;
-        cleanArgs[j] = NULL;
-
-        pid_t pid = fork();
-        if(pid==0){
-            if(inDirect==1){
-                int fd_in = open(infile,O_RDONLY);
-                if(fd_in<0){
-                    perror("file open error");
-                    exit(EXIT_FAILURE);
-                }
-                if(dup2(fd_in,STDIN_FILENO)<0){
-                    perror("dup2 error");
-                    close(fd_in);
-                    exit(EXIT_FAILURE);
-                }
-                close(fd_in);
-            }
-            if(outDirect==1){
-                int fd_out = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd_out < 0) {
-                    perror("open output file");
-                    exit(EXIT_FAILURE);
-                }
-                if (dup2(fd_out, STDOUT_FILENO) < 0) {
-                    perror("dup2 output");
-                    close(fd_out);
-                    exit(EXIT_FAILURE);
-                }
-                close(fd_out);
-            }
-            execvp(cleanArgs[0],cleanArgs);
-            perror("execvp failed");
-            exit(EXIT_FAILURE);
-        }
-        else if(pid>0){
-            if(!background){
-                int status;
-                waitpid(pid,&status,0);
-                // printf("exit status: %d\n",status);
-            } 
-            else {
-                // background single command: don't wait
-                printf("[background] pid: %d\n", pid);
-            }
-        }
-        else{
-            perror("fork failed");
-        }
-    }
-
-    return 0;
 }
